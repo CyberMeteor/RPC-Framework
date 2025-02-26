861280850072ef9c77fededc87c872618ea16140
Ruiyang Chen <im.ruiyangchen@gmail.com>
2025-02-26 16:45:42 -0500

    Implement thread pool utils

---
+++ rpc-core/src/main/java/com/alex/rpc/util/ThreadPoolUtils.java
@@ -0,0 +1,92 @@
+package com.alex.rpc.util;
+
+import cn.hutool.core.thread.ThreadFactoryBuilder;
+import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.*;
+
+@Slf4j
+public final class ThreadPoolUtils {
+    private static final Map<String, ExecutorService> THREAD_POOL_CACHE = new ConcurrentHashMap<>();
+    private static final int CPU_NUM = Runtime.getRuntime().availableProcessors();
+    private static final int CPU_INTENSIVE_NUM = CPU_NUM + 1;
+    private static final int IO_INTENSIVE_NUM = CPU_NUM * 2;
+    private static final int DEFAULT_KEEP_ALIVE_TIME = 60;
+    private static final int DEFAULT_QUEUE_SIZE = 128;
+
+    public static ExecutorService createCpuIntensiveThreadPool(String poolName) {
+        return createThreadPool(CPU_INTENSIVE_NUM, poolName);
+    }
+
+    public static ExecutorService createIoIntensiveThreadPool(String poolName) {
+        return createThreadPool(IO_INTENSIVE_NUM, poolName);
+    }
+
+    public static ExecutorService createThreadPool(
+            int corePoolSize,
+            String poolName
+    ) {
+        return createThreadPool(corePoolSize, corePoolSize, poolName);
+    }
+
+    public static ExecutorService createThreadPool(
+            int corePoolSize,
+            int maxPoolSize,
+            String poolName
+    ) {
+        return createThreadPool(corePoolSize, maxPoolSize, DEFAULT_KEEP_ALIVE_TIME, DEFAULT_QUEUE_SIZE, poolName);
+    }
+
+    public static ExecutorService createThreadPool(
+            int corePoolSize,
+            int maxPoolSize,
+            long keepAliveTime,
+            int queueSize,
+            String poolName
+    ) {
+        return createThreadPool(corePoolSize, maxPoolSize, keepAliveTime, queueSize, poolName, false);
+    }
+
+    public static ExecutorService createThreadPool(
+            int corePoolSize,
+            int maxPoolSize,
+            long keepAliveTime,
+            int queueSize,
+            String poolName,
+            boolean isDaemon
+    ) {
+        if (THREAD_POOL_CACHE.containsKey(poolName)) {
+            return THREAD_POOL_CACHE.get(poolName);
+        }
+
+         ExecutorService executorService = new ThreadPoolExecutor(
+                corePoolSize,
+                maxPoolSize,
+                keepAliveTime,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(queueSize),
+                 createThreadFactory(poolName, isDaemon)
+        );
+
+        THREAD_POOL_CACHE.put(poolName, executorService);
+        return executorService;
+    }
+
+    public static ThreadFactory createThreadFactory(String poolName) {
+        return createThreadFactory(poolName, false);
+    }
+
+    public static ThreadFactory createThreadFactory(String poolName, boolean isDaemon) {
+        ThreadFactoryBuilder threadFactoryBuilder = ThreadFactoryBuilder.create()
+                .setDaemon(isDaemon);
+
+        if (StrUtil.isBlank(poolName)) {
+            return threadFactoryBuilder.build();
+        }
+
+        return threadFactoryBuilder.setNamePrefix(poolName).build();
+    }
+}
