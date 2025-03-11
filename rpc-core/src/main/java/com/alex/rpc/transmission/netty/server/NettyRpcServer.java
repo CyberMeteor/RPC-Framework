80f0ea428568373306f785adb35db8da810cf7fe
Ruiyang Chen <im.ruiyangchen@gmail.com>
2025-03-10 23:55:19 -0400

    Implement netty rpc server

--- rpc-core/src/main/java/com/alex/rpc/transmission/netty/server/NettyRpcServer.java
+++ rpc-core/src/main/java/com/alex/rpc/transmission/netty/server/NettyRpcServer.java
@@ -1,12 +1,48 @@
 package com.alex.rpc.transmission.netty.server;
 
 import com.alex.rpc.config.RpcServiceConfig;
+import com.alex.rpc.constant.RpcConstant;
 import com.alex.rpc.transmission.RpcServer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class NettyRpcServer implements RpcServer {
     @Override
     public void start() {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<NioServerSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioServerSocketChannel channel) throws Exception {
+                            channel.pipeline().addLast(new StringDecoder());
+                            channel.pipeline().addLast(new StringEncoder());
+                            //channel.pipeline().addLast(new);
+                        }
+                    });
 
+            ChannelFuture channelFuture = bootstrap.bind(RpcConstant.SERVER_PORT).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("Server error.", e);
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 
     @Override
