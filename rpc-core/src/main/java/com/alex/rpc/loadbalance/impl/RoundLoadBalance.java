551d0719ebbdc7b05d9c6a98de38f66f04f849d4
Ruiyang Chen <im.ruiyangchen@gmail.com>
2025-03-13 22:30:45 -0400

    Implement round load balance

---
+++ rpc-core/src/main/java/com/alex/rpc/loadbalance/impl/RoundLoadBalance.java
@@ -0,0 +1,17 @@
+package com.alex.rpc.loadbalance.impl;
+
+import com.alex.rpc.loadbalance.LoadBalance;
+
+import java.util.List;
+
+public class RoundLoadBalance implements LoadBalance {
+    private int last = -1;
+
+    @Override
+    public String select(List<String> list) {
+        last++;
+        last = last % list.size();
+
+        return list.get(last);
+    }
+}
--- test-server/src/main/java/com/alex/server/Main.java
+++ test-server/src/main/java/com/alex/server/Main.java
@@ -1,10 +1,13 @@
 package com.alex.server;
 
+import cn.hutool.core.collection.ListUtil;
 import com.alex.api.User;
 import com.alex.api.UserService;
 import com.alex.rpc.config.RpcServiceConfig;
 import com.alex.rpc.dto.RpcReq;
 import com.alex.rpc.factory.SingletonFactory;
+import com.alex.rpc.loadbalance.LoadBalance;
+import com.alex.rpc.loadbalance.impl.RoundLoadBalance;
 import com.alex.rpc.proxy.RpcClientProxy;
 import com.alex.rpc.serialize.Serializer;
 import com.alex.rpc.serialize.impl.HessianSerializer;
@@ -14,6 +17,8 @@ import com.alex.rpc.transmission.netty.server.NettyRpcServer;
 import com.alex.rpc.transmission.socket.server.SocketRpcServer;
 import com.alex.server.service.UserServiceImpl;
 import org.apache.log4j.BasicConfigurator;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -25,18 +30,13 @@ public class Main {
 //
 //        rpcServer.start();
 
-//        Serializer serializer = SingletonFactory.getInstance(HessianSerializer.class);
-        Serializer serializer = SingletonFactory.getInstance(ProtostuffSerializer.class);
+        LoadBalance loadBalance = SingletonFactory.getInstance(RoundLoadBalance.class);
 
-        RpcReq rpcReq = RpcReq.builder()
-                .reqId("123321")
-                .interfaceName("qwert")
-                .paramTypes(new Class<?>[]{String.class, Long.class})
-                .build();
+        List<String> list = ListUtil.of("ip1:port1", "ip2:port2", "ip3:port3", "ip4:port4");
 
-        byte[] data = serializer.serialize(rpcReq);
-        RpcReq deserializedReq = serializer.deserialize(data, RpcReq.class);
-
-        System.out.println(deserializedReq);
+        for (int i = 0; i < 10; i++) {
+            String select = loadBalance.select(list);
+            System.out.println(select);
+        }
     }
 }
